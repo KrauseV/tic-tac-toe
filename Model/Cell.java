fa9623b5daf6732917aeaa520dc1379776213801
KrauseV <galder@gmail.com>
2022-07-11 00:10:52 +0400

    Add model classes

--- Model/Cell.java
+++ Model/Cell.java
@@ -16,22 +16,21 @@
 
 package Model;
 
-public class GameTable {
-    private final char[][] table = {
-            {' ', ' ', ' '},
-            {' ', ' ', ' '},
-            {' ', ' ', ' '}
-    };
+public class Cell {
+    private final int row;
 
-    public boolean isEmpty(final Cell cell) {
-        return table[cell.getRow()][cell.getCol()] == ' ';
+    public int getRow() {
+        return row;
     }
 
-    public char getSign(final Cell cell) {
-        return table[cell.getRow()][cell.getCol()];
+    public int getCol() {
+        return col;
     }
 
-    public void setSign(final Cell cell, final char sign) {
-        table[cell.getRow()][cell.getCol()] = sign;
+    public Cell(int row, int col) {
+        this.row = row;
+        this.col = col;
     }
+
+    private final int col;
 }
