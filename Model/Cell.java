d1932cdf2a428e7eb2ab356ab0bcb46bf84ac71e
KrauseV <galder@gmail.com>
2022-07-11 16:40:14 +0400

    Override toString methods for models classes

--- Model/Cell.java
+++ Model/Cell.java
@@ -18,6 +18,12 @@ package Model;
 
 public class Cell {
     private final int row;
+    private final int col;
+
+    public Cell(int row, int col) {
+        this.row = row;
+        this.col = col;
+    }
 
     public int getRow() {
         return row;
@@ -27,10 +33,11 @@ public class Cell {
         return col;
     }
 
-    public Cell(int row, int col) {
-        this.row = row;
-        this.col = col;
+    @Override
+    public String toString() {
+        return "Cell{" +
+                "row=" + row +
+                ", col=" + col +
+                '}';
     }
-
-    private final int col;
 }
