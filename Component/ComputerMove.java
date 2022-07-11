2d0259370b52b9dbe760c82b7d3c68c14f7487b5
KrauseV <galder@gmail.com>
2022-07-11 18:17:10 +0400

    Implement method make in ComputerMove class

--- Component/ComputerMove.java
+++ Component/ComputerMove.java
@@ -16,9 +16,24 @@
 
 package Component;
 
+import Model.Cell;
 import Model.GameTable;
+
+import java.util.Random;
 
 public class ComputerMove {
     public void make(GameTable gameTable) {
+        Random random = new Random();
+        while (true) {
+            final int col = random.nextInt(3);
+            final int row = random.nextInt(3);
+            final Cell randomCell = new Cell(row, col);
+            if (gameTable.isEmpty(randomCell)) {
+                gameTable.setSign(randomCell, 'O');
+
+                break;
+            }
+        }
+
     }
 }
