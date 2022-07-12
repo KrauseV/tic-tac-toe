8609853ac321f6e60045c6d2f1e8c0267492134b
KrauseV <galder@gmail.com>
2022-07-12 21:29:14 +0400

    Implement method Make in PlayerMove class

--- Component/PlayerMove.java
+++ Component/PlayerMove.java
@@ -16,9 +16,49 @@
 
 package Component;
 
+import Model.Cell;
 import Model.GameTable;
 
+import java.util.Scanner;
+
 public class PlayerMove {
+    int[][] gameMaket = new int[][]{
+            {7, 8, 9},
+            {4, 5, 6},
+            {1, 2, 3}
+    };
+
     public void make(GameTable gameTable) {
+
+        while (true) {
+            final Cell cell = getUserinput();
+            if (gameTable.isEmpty(cell)) {
+                gameTable.setSign(cell, 'X');
+                return;
+            } else {
+                System.out.println("Ячейка занята");
+            }
+        }
     }
+
+    private Cell getUserinput() {
+        String step = new Scanner(System.in).nextLine();
+        while (true) {
+            if (step.length() == 1) {
+                int userStep = Integer.parseInt(step);
+                if (userStep != 0) {
+                    for (int i = 0; i < gameMaket.length; i++) {
+                        for (int j = 0; j < gameMaket[i].length; j++) {
+                            if (userStep == gameMaket[i][j]) {
+                                return new Cell(i, j);
+                            }
+                        }
+
+                    }
+                } else System.out.println("Введите число от 1 до 9");
+            } else System.out.println("Введите число от 1 до 9");
+        }
+    }
+
+
 }
